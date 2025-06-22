70ca8e4dc443bb09c86ad960b24ea73b3fc14ed5
christopher <christop.pereira@hes-so.ch>
2025-06-22 14:20:21 +0200

    maj json-ld, icon et sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,15 +3,44 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Comptarial',
+  description: 'Comptarial – Fiduciaire moderne en Suisse. Services de comptabilité, paie, fiscalité, documents & conseil. Solutions sur mesure pour entrepreneurs et PME.'
 }
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
+    <html lang="fr">
+      <head>
+        <link rel="icon" href="https://comptarial.ch/logo.png" />
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "Organization",
+              "name": "Comptarial",
+              "url": "https://comptarial.ch",
+              "logo": "https://comptarial.ch/logo.png",
+              "description": "Fiduciaire moderne en Suisse. Services de comptabilité, paie, fiscalité, documents & conseil.",
+              "sameAs": [
+                "https://ch.linkedin.com/in/rosa-damasio-comptarial-19a148b6",
+                "https://www.facebook.com/comptarial",
+                "https://www.instagram.com/rosa.damasi.o"
+              ],
+              "contactPoint": [
+                {
+                  "@type": "ContactPoint",
+                  "telephone": "+41-22-794-04-32",
+                  "contactType": "customer service",
+                  "areaServed": "CH",
+                  "availableLanguage": ["fr"]
+                }
+              ],
+            }),
+          }}
+        />
+      </head>
       <body>{children}</body>
     </html>
   )
